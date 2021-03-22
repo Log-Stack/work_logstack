6bddf5b266796baad4617c2c8ff49cf3a0ad9551
logstack <jinho9613@naver.com>
2021-03-22 17:21:50 +0900

    authy models init

--- authy/models.py
+++ authy/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    date = models.DateField(auto_now_add=True)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=26)
+
+
+class TeamManager(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL)
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL)
+
+    name = models.CharField(max_length=25)
+    birth_day = models.DateField()
+    phone_number = models.CharField(max_length=50)
+    currently_employed = models.BooleanField(default=True)
+
