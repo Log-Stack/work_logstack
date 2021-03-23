5bbbb0a6bd796d758d9a531ac3a56838660335a9
logstack2 <ymh1791472@gmail.com>
2021-03-23 18:00:14 +0900

    Add work_log and work_hour

--- work_log/models.py
+++ work_log/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class WorkHour(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
-    start_time = models.DateTimeField(auto_now_add=True)
-    end_time = models.DateTimeField()
+    start_time = models.DateTimeField(default=timezone.now)
+    end_time = models.DateTimeField(blank=True, null=True)
 
 
 class WorkLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     create_time = models.DateTimeField(auto_now_add=True)
-    updated = models.BooleanField(default=False)
+    update_time = models.DateTimeField(auto_now=True)
+    is_updated = models.BooleanField(default=False)
--- work_log/urls.py
+++ work_log/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-
+from .views import start_working, end_working, work_log_list, work_log_write, work_log_detail, work_log_edit
 
 urlpatterns = [
+    path('start/', start_working, name='start_working'),
+    path('end/', end_working, name='end_working'),
+    path('list/', work_log_list, name='work_log_list'),
+    path('write/', work_log_write, name='work_log_write'),
+    path('edit/<int:pk>/', work_log_edit, name='work_log_edit'),
+    path('detail/<int:pk>/', work_log_detail, name='work_log_detail'),
 ]
