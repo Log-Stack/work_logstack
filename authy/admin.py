5fccae3bbd52b080014f79610b231a345ca990ff
yujin <iyoujine@gmail.com>
2021-03-23 12:43:30 +0900

    Register Authy Models in Adminsite

--- authy/admin.py
+++ authy/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from authy.models import Team, Profile, Position, TeamManager
+
+admin.site.register(Team)
+admin.site.register(Profile)
+admin.site.register(Position)
+admin.site.register(TeamManager)
