faf3ae1264089f0fc668ba0ca2109d578895321e
logstack <jinho9613@naver.com>
2021-03-23 11:23:22 +0900

    schedule model create

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Schedule(models.Model):
+    WORK_TYPES = ((1, 'work'), (2, 'vacation'),)
+    # 근무, 휴가 ,(추가사항) 외근, ...
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    start = models.TimeField()
+    end = models.TimeField()
+    work_type = models.IntegerField(choices=WORK_TYPES)
+
+
+class Schedule_permission(models.Model):
+    permission_TYPES = ((1, 'waiting'), (2, 'approved'), (3, 'reject'),)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    week_start_date = models.DateField()
+    permission_TYPES = models.IntegerField(choices=permission_TYPES)
