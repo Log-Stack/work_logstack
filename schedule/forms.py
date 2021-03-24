b54aa35753accd9f138cfe348d8eb781d0c7a7f6
logstack <jinho9613@naver.com>
2021-03-24 12:24:43 +0900

    schedule register date select complete

--- schedule/forms.py
+++ schedule/forms.py
@@ -5,42 +5,43 @@ from django.forms import ClearableFileInput
 
 
 class NewScheduleForm(forms.ModelForm):
-    week_start_date = forms.DateField()
+    week_start_date = forms.DateField(
+        widget=forms.DateInput(attrs={'class': "input", 'type': "date", 'id': 'week_start_date'}))
 
     sun_work_type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'select is-success'}),
                                       choices=Schedule.WORK_TYPES)
-    sun_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
-    sun_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
+    sun_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
+    sun_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
 
     mon_work_type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'select is-success'}),
                                       choices=Schedule.WORK_TYPES)
-    mon_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
-    mon_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
+    mon_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
+    mon_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
 
     tue_work_type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'select is-success'}),
                                       choices=Schedule.WORK_TYPES)
-    tue_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
-    tue_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
+    tue_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
+    tue_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
 
     wed_work_type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'select is-success'}),
                                       choices=Schedule.WORK_TYPES)
-    wed_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
-    wed_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
+    wed_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
+    wed_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
 
     thu_work_type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'select is-success'}),
                                       choices=Schedule.WORK_TYPES)
-    thu_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
-    thu_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
+    thu_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
+    thu_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
 
     fri_work_type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'select is-success'}),
                                       choices=Schedule.WORK_TYPES)
-    fri_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
-    fri_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
+    fri_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
+    fri_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
 
     sat_work_type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'select is-success'}),
                                       choices=Schedule.WORK_TYPES)
-    sat_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
-    sat_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}))
+    sat_start = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
+    sat_end = forms.TimeField(widget=forms.TimeInput(attrs={'class': "input", 'type': "time"}), required=False)
 
     class Meta:
         model = Schedule
--- schedule/views.py
+++ schedule/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect
 from django.template import loader
 
 from django.contrib.auth.models import User
@@ -36,6 +39,62 @@ def approved(request):
 
 @login_required
 def register_index(request):
+    if request.method == 'POST':
+        form = NewScheduleForm(request.POST)
+        print(request.POST)
+        print(form.errors)
+        if form.is_valid():
+            print('form valid pass')
+
+            week_start_date = form.cleaned_data.get('week_start_date')
+
+            sun_work_type = form.cleaned_data.get('sun_work_type')
+            sun_start = form.cleaned_data.get('sun_start')
+            sun_end = form.cleaned_data.get('sun_end')
+
+            mon_work_type = form.cleaned_data.get('mon_work_type')
+            mon_start = form.cleaned_data.get('mon_start')
+            mon_end = form.cleaned_data.get('mon_end')
+
+            tue_work_type = form.cleaned_data.get('tue_work_type')
+            tue_start = form.cleaned_data.get('tue_start')
+            tue_end = form.cleaned_data.get('tue_end')
+
+            wed_work_type = form.cleaned_data.get('wed_work_type')
+            wed_start = form.cleaned_data.get('wed_start')
+            wed_end = form.cleaned_data.get('wed_end')
+
+            thu_work_type = form.cleaned_data.get('thu_work_type')
+            thu_start = form.cleaned_data.get('thu_start')
+            thu_end = form.cleaned_data.get('thu_end')
+
+            fri_work_type = form.cleaned_data.get('fri_work_type')
+            fri_start = form.cleaned_data.get('fri_start')
+            fri_end = form.cleaned_data.get('fri_end')
+
+            sat_work_type = form.cleaned_data.get('sat_work_type')
+            sat_start = form.cleaned_data.get('sat_start')
+            sat_end = form.cleaned_data.get('sat_end')
+
+            print(week_start_date)
+
+            return redirect('schedule-register')
+    else:
+        user = request.user
+
+        selected_date = datetime.today().strftime("%Y-%m-%d")
+
+
+        template = loader.get_template('schedule_register.html')
+        form = NewScheduleForm()
+        context = {
+            'user': user,
+            'selected_date': selected_date,
+            'forms': form
+        }
+
+        return HttpResponse(template.render(context, request))
+
     user = request.user
     template = loader.get_template('schedule_register.html')
     form = NewScheduleForm()
@@ -45,20 +104,60 @@ def register_index(request):
     }
 
     return HttpResponse(template.render(context, request))
+
 
 @login_required
 def register_select_date(request, year, month, date):
-    user = request.user
-    selected_date = year + "-" + month + "-" + date
-    template = loader.get_template('schedule_register.html')
-    form = NewScheduleForm()
-    context = {
-        'user': user,
-        'selected_date': selected_date,
-        'forms': form
-    }
+    print('post pass')
+    if request.method == 'POST':
+        print('post pass')
+        form = NewScheduleForm(request.POST)
+        if form.is_valid():
+            week_start_date = form.cleaned_data.get('week_start_date')
 
-    return HttpResponse(template.render(context, request))
+            sun_work_type = form.cleaned_data.get('sun_work_type')
+            sun_start = form.cleaned_data.get('sun_start')
+            sun_end = form.cleaned_data.get('sun_end')
+
+            mon_work_type = form.cleaned_data.get('mon_work_type')
+            mon_start = form.cleaned_data.get('mon_start')
+            mon_end = form.cleaned_data.get('mon_end')
+
+            tue_work_type = form.cleaned_data.get('tue_work_type')
+            tue_start = form.cleaned_data.get('tue_start')
+            tue_end = form.cleaned_data.get('tue_end')
+
+            wed_work_type = form.cleaned_data.get('wed_work_type')
+            wed_start = form.cleaned_data.get('wed_start')
+            wed_end = form.cleaned_data.get('wed_end')
+
+            thu_work_type = form.cleaned_data.get('thu_work_type')
+            thu_start = form.cleaned_data.get('thu_start')
+            thu_end = form.cleaned_data.get('thu_end')
+
+            fri_work_type = form.cleaned_data.get('fri_work_type')
+            fri_start = form.cleaned_data.get('fri_start')
+            fri_end = form.cleaned_data.get('fri_end')
+
+            sat_work_type = form.cleaned_data.get('sat_work_type')
+            sat_start = form.cleaned_data.get('sat_start')
+            sat_end = form.cleaned_data.get('sat_end')
+
+            print(sat_work_type)
+
+            return redirect('schedule-register')
+    else:
+        user = request.user
+        selected_date = year + "-" + month + "-" + date
+        template = loader.get_template('schedule_register.html')
+        form = NewScheduleForm()
+        context = {
+            'user': user,
+            'selected_date': selected_date,
+            'forms': form
+        }
+
+        return HttpResponse(template.render(context, request))
 
 
 @login_required
