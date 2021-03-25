6ea43e4392e5373fdbdf2c56ec23c7707445b422
yujin <iyoujine@gmail.com>
2021-03-25 12:28:30 +0900

    Create Logout, Link Menus

--- authy/urls.py
+++ authy/urls.py
@@ -4,8 +4,7 @@ from rest_framework import routers
 from django.contrib.auth import views as authViews
 
 from authy import views
-from authy.views import index, CreateUserView, CreateTeamView, EditProfileView, ChangePWView, ChangePWDoneView,\
-    UserSearchView, SearchSelectView, UserDetailView, ProfileView
+from authy.views import index, CreateUserView, CreateTeamView, EditProfileView, ChangePWView, ChangePWDoneView, UserSearchView, SearchSelectView, UserDetailView, ProfileView
 
 router = routers.DefaultRouter()
 router.register(r'teams', views.TeamViewSet)
@@ -14,6 +13,7 @@ router.register(r'profiles', views.ProfileViewSet)
 
 urlpatterns = [
     path('login/', authViews.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', authViews.LogoutView.as_view(template_name='logged_out.html'), name='logout'),
    	path('', index, name='index'),
 
     # 팀장
