50dc45c066f46f88f9cf974b26a574fe7ea52fd2
yujin <iyoujine@gmail.com>
2021-04-07 18:06:49 +0900

    Edit Send Direct

--- directs/views.py
+++ directs/views.py
@@ -13,8 +13,24 @@ def directs_list(request):
     return HttpResponse(template.render(context,request))
 
 
+# @login_required
+# def SendDirect(request):
+#     from_user = request.user
+#     to_user_username = request.POST.get('to_user')
+#     body = request.POST.get('body')
+#
+#     if request.method == 'POST':
+#         to_user = User.objects.get(username=to_user_username)
+#         Message.send_message(from_user, to_user, body)
+#         return redirect('inbox')
+#     else:
+#         HttpResponseBadRequest()
+#
+#
 @login_required
 def directs_send(request):
+
+
 
     template = loader.get_template('directs_send.html')
     context = {
