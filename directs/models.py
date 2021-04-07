371babc18422ebfe4051becd4160d1fed3dd39b0
yujin <iyoujine@gmail.com>
2021-04-07 16:33:15 +0900

    Edit Message Model

--- directs/models.py
+++ directs/models.py
@@ -8,6 +8,7 @@ class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='from_user')
     recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='to_user')
+    title = models.CharField(max_length=200)
     body = models.TextField(max_length=1000, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
 
