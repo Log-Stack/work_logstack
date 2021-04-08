90315e66941e21726b4b3cb695f252550495f782
logstack <jinho9613@naver.com>
2021-04-08 14:46:09 +0900

    work_log, work_hour model 수정 schedule_register.html 수정

--- work_log/models.py
+++ work_log/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
@@ -5,7 +7,7 @@ from django.utils import timezone
 
 class WorkHour(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField(default=datetime.now())
     start_time = models.DateTimeField(default=timezone.now)
     end_time = models.DateTimeField(blank=True, null=True)
 
@@ -13,6 +15,6 @@ class WorkHour(models.Model):
 class WorkLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    create_time = models.DateTimeField(auto_now_add=True)
+    create_time = models.DateTimeField(default=timezone.now)
     update_time = models.DateTimeField(auto_now=True)
     is_updated = models.BooleanField(default=False)
