e7b0e082cfdcc5e6e47ec95c5ac70a79a74af2ff
yujin <iyoujine@gmail.com>
2021-04-09 11:38:00 +0900

    Add User Selection

--- directs/views.py
+++ directs/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import redirect
 from django.template import loader
+
+from directs.models import Message
 
 
 @login_required
@@ -27,16 +31,31 @@ def directs_list(request):
 #         HttpResponseBadRequest()
 #
 #
+
+
+
 @login_required
 def directs_send(request):
+    if request.method == "POST":
+        from_user = request.user
+        to_user = User.objects.get(pk=43)
+        title = request.POST.get('title')
+        body = request.POST.get('body')
+        team = request.POST.get('team')
+        user = request.POST.get('user')
+        print(title, body, team,  user)
+        Message.send_message(from_user,to_user,title,body)
 
-
+        return redirect('directlist')
+    else:
+        HttpResponseBadRequest()
 
     template = loader.get_template('directs_send.html')
     context = {
 
     }
     return HttpResponse(template.render(context,request))
+
 
 
 @login_required
