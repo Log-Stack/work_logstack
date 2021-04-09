bad8fb40bf7e14679ea6568c17ca92c1d96c8e39
yujin <iyoujine@gmail.com>
2021-04-09 11:04:26 +0900

    Register Message Model on Admin & Edit Message Method

--- directs/models.py
+++ directs/models.py
@@ -13,30 +13,31 @@ class Message(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
 
-    # def send_message(from_user, to_user, body):
-    #     sender_message = Message(
-    #         user=from_user,
-    #         sender=from_user,
-    #         recipient=to_user,
-    #         body=body,
-    #         )
-    #     sender_message.save()
-    #
-    #     recipient_message = Message(
-    #         user=to_user,
-    #         sender=from_user,
-    #         body=body,
-    #         recipient=from_user, )
-    #     recipient_message.save()
-    #     return sender_message
-    #
-    # def get_messages(user):
-    #     messages = Message.objects.filter(user=user).values('recipient').annotate(last=Max('date')).order_by('-last')
-    #     users = []
-    #     for message in messages:
-    #         users.append({
-    #             'user': User.objects.get(pk=message['recipient']),
-    #             'last': message['last'],
-    #             'unread': Message.objects.filter(user=user, recipient__pk=message['recipient'], is_read=False).count()
-    #         })
-    #     return users
+    def send_message(from_user, to_user, title, body):
+        sender_message = Message(
+            user=from_user,
+            sender=from_user,
+            recipient=to_user,
+            title=title,
+            body=body,
+            )
+        sender_message.save()
+
+        recipient_message = Message(
+            user=to_user,
+            sender=from_user,
+            body=body,
+            recipient=from_user, )
+        recipient_message.save()
+        return sender_message
+
+    def get_messages(user):
+        messages = Message.objects.filter(user=user).values('recipient').annotate(last=Max('date')).order_by('-last')
+        users = []
+        for message in messages:
+            users.append({
+                'user': User.objects.get(pk=message['recipient']),
+                'last': message['last'],
+                'unread': Message.objects.filter(user=user, recipient__pk=message['recipient'], is_read=False).count()
+            })
+        return users
